aac95e94455f2816e3f3fd2fdf8eca2d59d8faa8
Alexander Terbeznik <alexander.terbeznik@gmail.com>
2021-11-24 16:12:29 +0100

    remove colab script and notebook

--- copy_to_drive.py
+++
@@ -1,6 +0,0 @@
-import shutil
-
-source = "./"
-destination = "/Users/alex/gdrive_alex/Projekte/tsf/"
-shutil.copytree(source, destination, dirs_exist_ok=True)
-shutil.rmtree(f"{destination}src/__pycache__")
